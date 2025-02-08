1539ff8cd4a6679a9c1861228e0783b1fb6136fb
GhazanfarIjazOcta <ghazanfar@octathorn.com>
2025-02-08 17:54:49 +0500

    experience update

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,20 +1,32 @@
 export const experiences = [
-  // {
-  //   id: 1,
-  //   title: 'Web Developer',
-  //   company: "Tera Bit IT",
-  //   duration: "(Jan 2022 - Present)"
-  // },
+  {
+    id: 1,
+    title: 'Full Stack Developer',
+    company: "Octathorn Technologies NASTP",
+    duration: "(Aug 2024 - Present)"
+  },
   {
     id: 2,
+    title: 'Web Developer',
+    company: "Tera Bit IT",
+    duration: "(Jan 2023 - March 2024)"
+  },
+  {
+    id: 3,
     title: "FullStack Developer",
     company: "Fiverr (freelance)",
     duration: "(Aug 2022 - Nov 2023)"
   },
   {
-    id: 3,
-    title: " Intern as Web Developer",
+    id: 4,
+    title: "Intern as Web Developer",
     company: "Quest Lab",
     duration: "(June 2022 - July 2022)"
+  },
+  {
+    id: 5,
+    title: "Freelancer- Remote",
+    company: "Fiver/Upwork",
+    duration: "(Aug 2022 - Nov 2023)"
   }
 ]
